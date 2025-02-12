5f70af092f0ac127c4e4581347e43bacec597080
Sharuf <ali@gmail.com>
2025-02-12 22:43:41 +0530

    logout-api-implimentation

--- .venv/chatpdfbackend/api/views.py
+++ .venv/chatpdfbackend/api/views.py
@@ -41,7 +41,7 @@ class GenerativeView(APIView):
             data_list = PdfDataSerializer(data, many=True)
             text = data_list.data[len(data_list.data)-1]['contents']
             ans = querry_with_pdf(text,question)
-            print(str(ans))
+            # print(str(ans))
             return Response({"message":str(ans)}, status=status.HTTP_200_OK)
         except Exception as error:
             return Response({"error":str(error)} , status=status.HTTP_404_NOT_FOUND)
