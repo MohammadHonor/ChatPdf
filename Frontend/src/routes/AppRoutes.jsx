6ca1335f85ca560850d70f0a6391bb013ab7b288
Sharuf <ali@gmail.com>
2025-02-16 11:08:05 +0530

    login-fix

--- Frontend/src/routes/AppRoutes.jsx
+++ Frontend/src/routes/AppRoutes.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import App from "../App";
 import Dashboard from "../Dashboard.jsx";
 import { Register } from "../Components/Register.jsx";
-import Login from "../Components/login.jsx";
+import Login from "../Components/Login.jsx";
 export const AppRoutes = () => {
   return (
     <div className=" bg-[#212121] h-screen gap-2 border-blue-700 flex justify-center items-center">
